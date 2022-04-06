5217c692a9fc08e7499c7894ef137508d5acb257
Rahul Vikram <rahulvikram479@gmail.com>
2022-04-05 21:20:27 -0700

    python section added

---
+++ about-site/script.js
@@ -0,0 +1,4 @@
+$(function(){
+    $("#header").load("../global-func/header.html");
+    $("#footer").load("../global-func/footer.html");
+});
--- home/js/script.js
+++ home/js/script.js
@@ -1,4 +1,19 @@
+// Load header and footer components
 $(function(){
     $("#header").load("../global-func/header.html");
     $("#footer").load("../global-func/footer.html");
   });
+
+// Random background image for homepage
+var mainSect = document.getElementById('main-sect');
+let imgChoices = ["../img/img3.jpg", "../img/img.jpg", "../img/img2.jpg"];
+var bgImg = imgChoices[Math.floor(Math.random() * imgChoices.length)];
+mainSect.style.backgroundImage = `linear-gradient(to bottom, rgba(0,0,0,0.58) 0%,rgba(0,0,0,0.66) 100%), url("${bgImg}")`;
+
+// Toggles 'scrolled' class on header to change it's background color
+$(function() {
+  $(document).scroll(function() {
+    var $nav = $(".header-bar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
